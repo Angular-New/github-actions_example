65fa43f7e5c6259c48e2fa07e462c9ae3df51f48
Verda Muso <gearmobile@gmail.com>
2024-03-11 22:35:51 +0300

    event-filters: update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'gha-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'github-actions_example';
